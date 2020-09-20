ac07ef203e6d2d5ddfb7348f01e5c98d79194170
Eric Gonzalez <eric.gonz.gomez@gmail.com>
2020-09-19 19:02:32 -0500

    Adding tests for merge lists uniques sample repo.

--- merge-lists-uniques-sample/main_test.go
+++ merge-lists-uniques-sample/main_test.go
@@ -1,7 +1,21 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestMain(testing *testing.T) {
+func TestMain(t *testing.T) {
+	expectedList := []string{"Matt", "Henry"}
+	uniqueList := uniqueNames([]string{"Matt", "Henry"}, []string{})
 
+	if reflect.DeepEqual(expectedList, uniqueList) == false {
+		t.Errorf("Failed to retrieve correct list, expected %v, and got %v", expectedList, uniqueList)
+	}
+
+	anotherUniqueLIst := uniqueNames([]string{}, []string{"Matt", "Henry"})
+
+	if reflect.DeepEqual(expectedList, anotherUniqueLIst) == false {
+		t.Errorf("Failed to retrieve correct list, expected %v, and got %v", expectedList, anotherUniqueLIst)
+	}
 }
